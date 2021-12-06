2f65078783fde1d88a1f758b7fb86c29252f4686
CodeCraftPlugin <86466022+CodeCraftPlugin@users.noreply.github.com>
2021-12-06 19:26:40 +0530

    Adding Texture and json files

--- src/main/java/me/codecraft/tut/block/advance/PepperPlantBlock.java
+++ src/main/java/me/codecraft/tut/block/advance/PepperPlantBlock.java
@@ -3,27 +3,16 @@ package me.codecraft.tut.block.advance;
 import me.codecraft.tut.item.ModItem;
 import net.minecraft.block.CropBlock;
 import net.minecraft.item.ItemConvertible;
-import net.minecraft.state.property.IntProperty;
-import net.minecraft.state.property.Properties;
 
 public class PepperPlantBlock extends CropBlock {
 
-    public static final IntProperty AGE = Properties.AGE_5;
-
-
     public PepperPlantBlock(Settings settings) {
         super(settings);
-
-
     }
 
     @Override
     protected ItemConvertible getSeedsItem() {
         return ModItem.PEPPER_SEED;
-    }
-    @Override
-    public int getMaxAge() {
-        return 5;
     }
 
 
